991dc5e073cfd99f003574f28680c7d79a055ef2
Ameen Alade <ameen.alade@VentureGardenGroup.Local>
2020-11-14 16:49:46 +0100

    fixed CORS issue

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,13 +1,14 @@
 import Axios from "axios";
 
-
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const API_URL = 'https://reqres.in/api'
 
 const api = Axios.create({
-    baseURL: API_URL,
+    baseURL: proxyurl+API_URL,
     headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        // 'Access-Control-Allow-Origin': API_URL
     }
 });
 

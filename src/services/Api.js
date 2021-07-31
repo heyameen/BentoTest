0c07a775d8d56308251bb9730ef94a5f6ffcbafc
Ameen Alade <alade.ameen@venturegardengroup.com>
2021-07-31 09:40:02 +0100

    fixed api issue on the app

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,10 +1,10 @@
 import Axios from "axios";
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const API_URL = 'https://reqres.in/api'
 
 const api = Axios.create({
-    baseURL: proxyurl+API_URL,
+    baseURL: API_URL,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
